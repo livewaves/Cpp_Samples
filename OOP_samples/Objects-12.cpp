3af16ec7bc4182d221152f577b3b113061dafb77
livewaves <hsnhejazin@gmail.com>
2024-02-20 17:14:04 +0100

    hierarchical inheritance

---
+++ OOP_samples/Objects-12.cpp
@@ -0,0 +1,45 @@
+// Hierarchical Inheritance
+#include <iostream>
+using namespace std;
+// many derived classes from a single base class
+
+// Derived class is formed from multiple base classes
+#include <iostream>
+using namespace std;
+
+// Base class
+class Vehicle {
+public:
+    void drive() {
+        cout << "Vehicle is being driven." << endl;
+    }
+};
+
+// Derived class Car inheriting from Vehicle
+class Car : public Vehicle {
+public:
+    void honk() {
+        cout << "Car is honking." << endl;
+    }
+};
+
+// Derived class Bike inheriting from Vehicle
+class Bike : public Vehicle {
+public:
+    void ringBell() {
+        cout << "Bike is ringing bell." << endl;
+    }
+};
+
+int main() {
+    Car myCar;
+    Bike myBike;
+
+    myCar.drive(); // Accessing drive() from Vehicle through Car
+    myCar.honk();  // Accessing honk() from Car
+
+    myBike.drive();    // Accessing drive() from Vehicle through Bike
+    myBike.ringBell(); // Accessing ringBell() from Bike
+
+    return 0;
+}
