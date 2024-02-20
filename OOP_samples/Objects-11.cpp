60a548f7cac4ebcb10af2b51879931d2d2747c01
livewaves <hsnhejazin@gmail.com>
2024-02-20 17:12:44 +0100

    multilevel inheritance

---
+++ OOP_samples/Objects-11.cpp
@@ -0,0 +1,46 @@
+// Multilevel Inheritance
+#include <iostream>
+using namespace std;
+
+// Derived class is formed from multiple base classes
+class Shape1
+{
+    protected:
+        int width;
+
+    public:
+    void setwidth(int w)
+    {
+        width = w;
+    }                 
+};
+
+class Shape2: public Shape1
+{
+    protected:
+        int height;
+    public:    
+        void setheight(int h)
+        {
+            height = h;
+        }    
+};
+
+// child has access to public members of the parent
+class Rectangle: public Shape2
+{
+    public:
+        int getarea()
+        {
+            return (width * height);
+        }
+};
+
+int main()
+{
+    Rectangle op;
+    op.setwidth(5);
+    op.setheight(6);
+    cout << "Area is : " <<op.getarea() << endl;  
+    return 0;
+}
