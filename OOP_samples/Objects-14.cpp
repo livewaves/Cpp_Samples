9e1a69becc2c72d51c21de1abb55e159dff20d19
Hejazinezhad.SH <82455942+livewaves@users.noreply.github.com>
2024-02-21 08:45:14 +0100

    polymorphism example

--- OOP_samples/Objects-14.cpp
+++ OOP_samples/Objects-14.cpp
@@ -4,11 +4,11 @@ using namespace std;
 // inheritance, encapsulation and polymorphism are pillars of OOP
 
 // prototype of display function is identical in A and B classes
-//Compile time polimorphism refers to the ability of the compiler to select the appropriate 
+// Compile time polymorphism refers to the ability of the compiler to select the appropriate 
 // function or method to call at compile time based on the types 
 // involved in the function call.
 // run time polimorphism selects the function in runtime and it is more flexible 
-// than complie time polimorphism
+// than complie time polymorphism
 class A{
     int a;
     public:
