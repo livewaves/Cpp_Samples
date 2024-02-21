73b8cdc098fc675e3f297bfdceaa737e84b635bd
Hejazinezhad.SH <82455942+livewaves@users.noreply.github.com>
2024-02-21 09:05:12 +0100

    runtime polymorphism

--- OOP_samples/Objects-16.cpp
+++ OOP_samples/Objects-16.cpp
@@ -1,14 +1,15 @@
-// Polymorphism
+// runtime Polymorphism
 #include <iostream>
 using namespace std;
 // inheritance, encapsulation and polymorphism are pillars of OOP
 
 // prototype os display function is identical in A and B classes
-//Compile time polimorphism refers to the ability of the compiler to select the appropriate 
+// Compile time polymorphism refers to the ability of the compiler to select the appropriate 
 // function or method to call at compile time based on the types 
 // involved in the function call.
-// run time polimorphism selects the function in runtime and it is more flexible 
-// than complie time polimorphism
+// run time polymorphism selects the function in runtime and it is more flexible 
+// than complie time polymorphism
+
 class A{
     int a;
     public:
@@ -48,4 +49,7 @@ int main()
     s = &c;
     s ->display();
     return 0;
+    // The pointer can be equal to a, b and c.
+    // Depending on the pointer, different display function
+    // are called
 }
