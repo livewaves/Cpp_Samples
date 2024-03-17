3251fb6f37058b9df1fd17a15fe7b53fc7af42a6
livewaves <hsnhejazin@gmail.com>
2024-03-17 11:30:49 +0100

    multimap upper bound

---
+++ multimap/multimap-06-2.cpp
@@ -0,0 +1,18 @@
+// multimap upper bound
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+int main()
+{
+    multimap <int, int> mp;
+    mp.insert({1, 23});
+    mp.insert({2, 10});
+    mp.insert({1, 30});
+    mp.insert({3, 22});
+    mp.insert({1, 32});
+
+    auto it = mp.upper_bound(2);
+    cout << "The upper bound of 2 is ";
+    cout << (*it).first << "    " << (*it).second << endl;
+}
