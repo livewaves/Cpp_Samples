dd459ea2ae08cd6cf20e7ff6bb12639e4bcfb405
livewaves <hsnhejazin@gmail.com>
2024-03-17 11:10:22 +0100

    find

---
+++ multimap/multimap-01-1.cpp
@@ -0,0 +1,21 @@
+// find 
+#include <iostream>
+using namespace std;
+#include <map>
+
+int main()
+{
+    multimap<int, int> mp;
+    mp.insert({2, 10});
+    mp.insert({1, 20});
+    mp.insert({3, 30});
+    mp.insert({1, 40});
+
+    for (auto itr  = mp.find(1); itr!=mp.end();itr++)
+    {
+        // cout << itr ->first << endl;
+        // cout << itr ->second<< endl;
+        cout << itr ->first << "    ";
+        cout << itr ->second<< endl;        
+    }
+}
