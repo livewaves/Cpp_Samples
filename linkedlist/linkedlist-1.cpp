192a6ff60906e51ee216f53766c96e4ed2004714
livewaves <hsnhejazin@gmail.com>
2024-03-18 17:28:48 +0100

    linkedlist defnition

---
+++ linkedlist/linkedlist-1.cpp
@@ -0,0 +1,4 @@
+// linkedlist definition
+// a seq of data structures connected to each other through lists
+// a list of links. each link has a data part and a ref (pointer) to the next list
+// last node, points to null. link is ended.
