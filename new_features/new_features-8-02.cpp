82e1240d6dec869401fe26ba01da5aef6563b8e9
livewaves <hsnhejazin@gmail.com>
2024-04-04 11:13:44 +0200

    different types of references to bind to the return value of a function

---
+++ new_features/new_features-8-02.cpp
@@ -0,0 +1,21 @@
+// different types of references (const int&, const int&&,
+// and int &&) to bind to the return value of a function
+#include <iostream>
+using namespace std;
+
+// as x is a temporary value, return of A() is an rvalue
+// we coould use a normal lvalue reference to bind temporary object if it is a constant
+int A()
+{
+    int x = 2;
+    return x;   
+}
+
+int main()
+{
+    cout << A() << endl;
+    const int& val1 = A();
+    const int&& val2 = A();
+    int && val3 = A();
+    return 0;
+}
